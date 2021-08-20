d0e84cf3c95ebd31a26b580176c795d9c597731f
iykethe1st <69741500+iykethe1st@users.noreply.github.com>
2021-08-20 10:22:17 +0100

    Update urls.py

--- myfirstapp/urls.py
+++ myfirstapp/urls.py
@@ -2,7 +2,7 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('',views.myfunctioncall,name="index"),
+    path('',views.resume,name="resume"),
     path('submitmyform',views.submitmyform,name='submitmyform'),
     path('resume', views.resume,name='resume'),
     path('success', views.success, name='success')
